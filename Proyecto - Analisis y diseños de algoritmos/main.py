ce359556c8550b5d58da6b3f56eb000700df8180
TestBox44 <iceglace12@hotmail.com>
2022-07-24 20:07:56 -0500

    cuarto cambio

--- Proyecto - Analisis y diseños de algoritmos/main.py
+++ Proyecto - Analisis y diseños de algoritmos/main.py
@@ -3,3 +3,11 @@ print("hola soy sebastian")
 print("HOLA #2")
 print("Hola soy Richard")
 print("hello")
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+    def imprimirDatos(self):
+        print("El nombre de la persona es ",self.nombre)
+persona1=Persona("Jose",22)  #Persona persona1=new Persona()
+persona1.imprimirDatos()
