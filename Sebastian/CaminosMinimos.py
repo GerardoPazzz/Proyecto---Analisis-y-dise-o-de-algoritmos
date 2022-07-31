7065ca7553cb85957cb48e4b354292dc304a56ba
TestBox44 <iceglace12@hotmail.com>
2022-07-31 05:10:04 -0500

    Segunda version del algoritmo Dijkstra y avance del sistema de representacion grafica(Sebastian)

--- Sebastian/CaminosMinimos.py
+++ Sebastian/CaminosMinimos.py
@@ -1,9 +1,33 @@
-#Constantes
+# Constantes
 """Valor 'Infinito'"""
 max_Int=2**63-1
-#Definicion de Funciones
-"""Primera version de algoritmo que utiliza una matriz de adyacencia y un valor maximo para obtener el camino minimo"""
+
+# Definicion de Funciones
+
+# Funciones de graficación
+def pintar_grafo():
+    """Funcion para pintar en consola, con objetivo de pruebas"""
+    pass
+
+# Funciones de apoyo
+def convertir_grid(grid):
+    """Funcion para convertir un grid de pesos en un grafo (diccionario de nodos)"""
+    grafo = {}
+    direcciones = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    for i, fila in enumerate(grid):
+        for j, columna in enumerate(fila):
+            nodos_vecinos = []
+            for x, y in direcciones:
+                newx, newy = x+i, y+j
+                if 0 <= newx < len(fila) and 0 <= newy < len(grid):
+                    nodos_vecinos.append(((newx, newy), grid[newx][newy]))
+            grafo[(i, j)] = nodos_vecinos
+    return grafo
+
+# Algoritmos de caminos minimos
 def Dijkstra(inicio,final,mAd):
+    """Primera version de algoritmo que utiliza una matriz de adyacencia
+    y un valor maximo para obtener el camino minimo"""
     # Variables
     costo_minimo=len(mAd)*[max_Int]
     recorrido_minimo=len(mAd)*[0]
@@ -34,3 +58,37 @@ def Dijkstra(inicio,final,mAd):
         camino+=","
     camino=camino[::-1]
     return camino
+
+def Dijkstra_v2(inicio,final,grafo):
+    """Segunda version de algoritmo que utiliza un diccionario con los nodos del grafo
+    para hallar la lista de nodos(puntos) que conforman el camino minimo. No requiere
+    un tamaño maximo o 'infinito'."""
+    # Variables
+    costo_minimo = {inicio: 0}
+    recorrido_de_nodos = {inicio: None}
+    nodos_visitados = []
+    nodo_actual = inicio
+    # Proceso
+    # Bucle con limite que recorrera como maximo todos los nodos del grafo para hallar el camino minimo
+    while nodo_actual is not None:
+        nodos_visitados.append(nodo_actual)
+        if nodo_actual == final:  # Detener bucle si ya se alcanzo el nodo final
+            break
+        for nodo_vecino in grafo[nodo_actual]:
+            punto_vecino, costo_de_vecino = nodo_vecino
+            if punto_vecino not in costo_minimo or costo_minimo[nodo_actual]+costo_de_vecino < costo_minimo[punto_vecino]:
+                costo_minimo[punto_vecino] = costo_minimo[nodo_actual]+costo_de_vecino
+                recorrido_de_nodos[punto_vecino] = nodo_actual
+        min = None
+        nodo_actual = None
+        for nodo in costo_minimo:
+            if nodo not in nodos_visitados:
+                min = costo_minimo[nodo] if min is None else min
+                if costo_minimo[nodo] <= min:  # Elegir un nuevo nodo a evaluar segun el minimo costo
+                    min = costo_minimo[nodo]
+                    nodo_actual = nodo
+    camino = []
+    while nodo_actual is not None:
+        camino.append(nodo_actual)
+        nodo_actual=recorrido_de_nodos[nodo_actual]
+    return camino[::-1]
--- Sebastian/pruebadealgoritmos.py
+++ Sebastian/pruebadealgoritmos.py
@@ -4,4 +4,14 @@ matrizPrueba=[[0,4,8,max_Int,max_Int],
               [8,1,0,4,2],
               [max_Int,2,4,0,7],
               [max_Int,max_Int,2,7,0]]
-print(Dijkstra(0,4,matrizPrueba))
+matrizPrueba2=[[max_Int,4,2,max_Int,max_Int,max_Int],
+               [4,max_Int,1,5,max_Int,max_Int],
+               [2,1,max_Int,8,10,max_Int],
+               [max_Int,5,8,max_Int,2,6],
+               [max_Int,max_Int,10,2,max_Int,2],
+               [max_Int,max_Int,max_Int,6,2,max_Int]]
+grid=[[0,0,0],
+      [0,5,0],
+      [0,0,0]]
+print(Dijkstra_v2((0,0),(2,2),convertir_grid(grid)))
+
