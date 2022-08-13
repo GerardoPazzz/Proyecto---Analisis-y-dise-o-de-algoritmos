9b438bbbd8e0b78c531c2f725357b09eb78710ca
TestBox44 <iceglace12@hotmail.com>
2022-08-13 05:11:12 -0500

    Retoques en ventanas (Datos)

--- Sebastian/ventana.py
+++ Sebastian/ventana.py
@@ -11,7 +11,7 @@ ventana = tkinter.Tk()
 #txtfield=tkinter.Entry(ventana,font="Arial 20")
 #txtfield.pack()
 
-activado=False
+buscando=False
 
 #Configuraciones del editor de mapa (Grafo)
 GRID_ALTO=26
@@ -25,6 +25,7 @@ peso_nodo_actual=1
 
 #CODIGO PARA DIJKSTRA
 grid = [[peso_nodo_actual for i in range(GRID_ANCHO)]for j in range(GRID_ALTO)]
+inicio, final = ((0, 0), (20, 20))  # Configuracion del punto de inicio y final
 lista_graficos_nodos=[]
 #Funciones
 def pintar_tile_en_canvas(x,y,tile):
@@ -41,9 +42,11 @@ def repintar_tile(event):
     #print(grid)
     pass
 def iniciar_busqueda():
+    boton_iniciar_busqueda.config(bg="red",text="PAUSAR")
+    global buscando
+    buscando=True
     tiempo=time.time()*1000
     grafo = convertir_grid(grid)
-    inicio, final = ((0, 0), (20, 20))  # Configuracion del punto de inicio y final
     costo_minimo = {inicio: 0}
     recorrido_de_nodos = {inicio: None}
     nodos_visitados = []
@@ -56,6 +59,9 @@ def iniciar_busqueda():
     while nodo_actual != final:
         costo_minimo, recorrido_de_nodos, nodos_visitados, nodo_actual, camino = \
             Dijkstra_v3(costo_minimo, recorrido_de_nodos, nodos_visitados, nodo_actual, grafo)
+        label_nodo_evaluado.config(text=f"({nodo_actual[0]},{nodo_actual[1]})")
+        label_nodos_visitados.config(text=f"{len(nodos_visitados)}")
+        label_nodos_camino.config(text=f"{len(camino)}")
         while len(lista_graficos_nodos) < len(camino):
             lista_graficos_nodos.append(canvas_grafo.create_oval(-15,-15,0,0, fill="blue",tags="nodo_camino"))
         canvas_grafo.moveto("nodo_camino",-15,-15)
@@ -68,7 +74,8 @@ def iniciar_busqueda():
         #print(camino)
         #time.sleep(0.1)
         ventana.update()
-    print("Camino hallado",time.time()*1000-tiempo,"ms")
+    label_ultima_evaluacion.config(text=f"{time.time()*1000-tiempo:.0f}")
+    boton_iniciar_busqueda.config(bg="green", text="INICIAR\nBÚSQUEDA")
     pass
 def reiniciar():
     lista_graficos_nodos.clear()
@@ -103,27 +110,48 @@ canvas_grafo.pack(side=tkinter.TOP, padx=(5, 5), pady=(5, 0))
 # Frame de configuracion de grafo
 configuracion_de_grafo = tkinter.Frame(framegrafo, bg=GRIS_OSCURO,highlightbackground="gray", highlightthickness=1)
 configuracion_de_grafo.config(width=150, height=120)
-configuracion_de_grafo.pack(side=tkinter.LEFT, padx=(5, 5), pady=5)
+configuracion_de_grafo.pack(side=tkinter.LEFT, padx=(5, 5), pady=5,fill=tkinter.Y)
 
 tkinter.Label(configuracion_de_grafo,text="CONFIGURACIÓN DE GRAFO",fg="white",bg=GRIS_OSCURO,font=fuente_titulo).grid(row=0,columnspan=2)
 tkinter.Label(configuracion_de_grafo,text="Punto inicial:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=1,column=0,sticky="e")
 tkinter.Entry(configuracion_de_grafo,width=8).grid(row=1,column=1,sticky="w")
 tkinter.Label(configuracion_de_grafo,text="Punto final:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=2,column=0,sticky="e")
 tkinter.Entry(configuracion_de_grafo,width=8).grid(row=2,column=1,sticky="w")
-tkinter.Label(configuracion_de_grafo,text="Nodos por lado:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=3,column=0,sticky="e")
+tkinter.Label(configuracion_de_grafo,text="Ancho x Alto:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=3,column=0,sticky="e")
+tkinter.Entry(configuracion_de_grafo,width=8).grid(row=3,column=1,sticky="w")
 tkinter.Label(configuracion_de_grafo,text="Tamaño de imagen:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=4,column=0,sticky="e")
+label_tamaño_imagen=tkinter.Label(configuracion_de_grafo,text="16 px",fg="white",bg=GRIS_OSCURO,font=fuente_opciones)
+label_tamaño_imagen.grid(row=4,column=1,sticky="w")
 tkinter.Label(configuracion_de_grafo,text="Mostrar rejilla",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=5,column=0,sticky="e")
 
 # Frame de configuracion de busqueda
 configuracion_de_busqueda = tkinter.Frame(framegrafo, bg=GRIS_OSCURO,highlightbackground="gray", highlightthickness=1)
 configuracion_de_busqueda.config(width=150, height=120)
 configuracion_de_busqueda.pack(side=tkinter.LEFT, padx=(0, 5), pady=5)
+
+tkinter.Label(configuracion_de_busqueda,text="            CONFIGURACIÓN DE BÚSQUEDA            ",fg="white",bg=GRIS_OSCURO,font=fuente_titulo).grid(row=0,columnspan=2)
+tkinter.Label(configuracion_de_busqueda,text="Nodo evaluado:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=1,column=0,sticky="e")
+label_nodo_evaluado=tkinter.Label(configuracion_de_busqueda,text="(0,0)",fg="white",bg=GRIS_OSCURO,font=fuente_opciones)
+label_nodo_evaluado.grid(row=1,column=1,sticky="w")
+tkinter.Label(configuracion_de_busqueda,text="Tiempo de retraso(ms):",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=2,column=0,sticky="e")
+tkinter.Entry(configuracion_de_busqueda,width=8).grid(row=2,column=1,sticky="w")
+tkinter.Label(configuracion_de_busqueda,text="Número de nodos visitados:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=3,column=0,sticky="e")
+label_nodos_visitados=tkinter.Label(configuracion_de_busqueda,text="0",fg="white",bg=GRIS_OSCURO,font=fuente_opciones)
+label_nodos_visitados.grid(row=3,column=1,sticky="w")
+tkinter.Label(configuracion_de_busqueda,text="Número de nodos de camino mínimo:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=4,column=0,sticky="e")
+label_nodos_camino=tkinter.Label(configuracion_de_busqueda,text="0",fg="white",bg=GRIS_OSCURO,font=fuente_opciones)
+label_nodos_camino.grid(row=4,column=1,sticky="w")
+tkinter.Label(configuracion_de_busqueda,text="Ultimo tiempo de evaluación:",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=5,column=0,sticky="e")
+label_ultima_evaluacion=tkinter.Label(configuracion_de_busqueda,text="0",fg="white",bg=GRIS_OSCURO,font=fuente_opciones)
+label_ultima_evaluacion.grid(row=5,column=1,sticky="w")
+tkinter.Label(configuracion_de_busqueda,text="Mostrar camino mínimo",fg="white",bg=GRIS_OSCURO,font=fuente_opciones).grid(row=6,column=0,sticky="e")
+
 # Boton de reiniciar
 boton_reiniciar = tkinter.Button(framegrafo, text="REINICIAR", bg="yellow",command=reiniciar)
 boton_reiniciar.config(width=14, height=3)
 boton_reiniciar.pack(side=tkinter.TOP, padx=(0, 3), pady=5)
 # Boton de iniciar busqueda
-boton_iniciar_busqueda = tkinter.Button(framegrafo, text="INICIAR \nBUSQUEDA", bg="green",command=iniciar_busqueda)
+boton_iniciar_busqueda = tkinter.Button(framegrafo, text="INICIAR\nBÚSQUEDA", bg="green",command=iniciar_busqueda)
 boton_iniciar_busqueda.config(width=14, height=3)
 boton_iniciar_busqueda.pack(side=tkinter.BOTTOM, padx=(0, 3), pady=(0, 5))
 
